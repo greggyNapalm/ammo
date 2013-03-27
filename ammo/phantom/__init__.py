66be5973fa6be0e955321b469f016b191f4bdce3
Gregory Komissarov <gregory.komissarov@gmail.com>
2013-03-27 15:57:19 +0400

    More flexible Phantom ammo compiler method added.

--- ammo/phantom/__init__.py
+++ ammo/phantom/__init__.py
@@ -9,17 +9,23 @@ Functions to generate raw HTTP request.
 """
 
 import io
-import sys
+#import sys
 import httplib
-import urllib
+#import urllib
 
 
 class HttpCompiler(object):
-    def __init__(self, cntx_up=None, logger=None):
+    version = '1.0'
+
+    def __init__(self, cntx_up=None):
+        self.method = None
+        self.qs = None
+        self.headers = {}
         self.defaults = {
             'method': 'GET',
             'Host': 'target.on-fire.io',
             'kwargs': {},
+            #'body': '{body}',
         }
         self.cntx = self.defaults
         if cntx_up:
@@ -29,8 +35,24 @@ class HttpCompiler(object):
         bio = io.BytesIO()
         bio.sendall = bio.write
         conn.sock = bio
-        conn.request(self.cntx['method'], '%s', **self.cntx['kwargs'])
+        conn.request(self.cntx['method'], '{qs}', **self.cntx['kwargs'])
         self.tmpl = bio.getvalue().decode('utf-8')
 
-    def build_req(self, qs):
+    def build_req(self, qs, body):
         return self.tmpl % qs
+
+    def from_dict(self, **kwargs):
+        host = kwargs.get('Host', None) or self.headers['Host']
+        method = kwargs.get('method', None) or self.method
+        qs = kwargs.get('qs', None) or self.qs
+        kw = {
+            'headers': kwargs.get('headers', None) or self.headers,
+            'body': kwargs.get('body', None) or self.body,
+        }
+
+        conn = httplib.HTTPConnection(host)
+        bio = io.BytesIO()
+        bio.sendall = bio.write
+        conn.sock = bio
+        conn.request(method, qs, **kw)
+        return bio.getvalue().decode('utf-8')
